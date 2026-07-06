5fad2f945dcb1ed369f4a5bb65ddf80c3e6909de
Pavos21 <metalmasterboy@gmail.com>
2026-07-06 20:15:00 +0200

    Shadow

--- src/components/moleculs/Notification.tsx
+++ src/components/moleculs/Notification.tsx
@@ -32,7 +32,7 @@ const Notification = () => {
           href="https://docs.google.com/forms/d/e/1FAIpQLSeSx8uyh53Qx0yqdSDkvOSuEgm4k9fj8FtPa4HFOb-ydP-N8w/viewform?usp=header"
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-darkPink text-beige text-3xl rounded-[8px] mt-4 px-6 py-3 inline-block text-center font-semibold transition hover:bg-beige hover:text-darkPink duration-300"
+          className="bg-darkPink text-beige text-3xl rounded-[8px] shadow-md mt-4 px-6 py-3 inline-block text-center font-semibold transition hover:bg-beige hover:text-darkPink duration-300"
         >
           CHCI UPOZORNĚNÍ
         </a>
